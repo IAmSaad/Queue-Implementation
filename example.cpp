bb9f22d5e43b606ee905e2e80a362ef0c281b7ae
IAmSaad <saadmahmoodmughal@gmail.com>
2016-04-09 00:13:12 +0500

    example file
    showing the use of given functions included in the library

---
+++ example.cpp
@@ -0,0 +1,24 @@
+#include"Queue.h"
+#include<iostream>
+
+using namespace std
+
+int main (){
+	
+	Queue people_outside_the_bank;
+	
+	people_outside_the_bank.enqueue(10);
+	people_outside_the_bank.enqueue(20);
+	people_outside_the_bank.enqueue(30);
+	
+	cout<<people_outside_the_bank.front()<<endl;
+	cout<<people_outside_the_bank.back()<<endl;
+	
+	people_outside_the_bank.display();
+	
+	return 0;
+}
+
+
+
+
